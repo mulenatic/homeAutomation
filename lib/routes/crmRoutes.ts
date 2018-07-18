c3228319db97359e2d76af0b9f9a52b6c5693295
mulenatic <mulenatic@gmail.com>
2018-07-18 19:03:53 +0200

    Added paths for several crud actions.

--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -9,6 +9,39 @@ export class Routes {
                     message: 'GET request successfull!!!'
                 })
             });
+
+        // Contact
+        app.route('/contact')
+            .get((req: Request, res: Response) => {
+                // Get all contacts
+                res.status(200).send({
+                    message: 'GET request successfull'
+                });
+            });
+	    .post((req: Request, res: Response) => {
+            // Create new contact
+            res.status(200).send({
+                message: 'POST request successfull'
+            })
+        });
+
+        // Contact detail
+        app.route('/contact/:contactId')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'GET request successfull'
+                })
+            })
+            .put((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'PUT request successfull'
+                })
+            })
+            .delete((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'DELETE request successfull'
+                })
+            });
     }
 }
 
