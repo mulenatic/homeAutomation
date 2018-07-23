a789e0d9170bb2e4b91778909cb845d2d2807798
mulenatic <mulenatic@gmail.com>
2018-07-23 11:19:21 +0200

    Created table in measurement Overview with links, have to create detail pages.

--- app_client/app.js
+++ app_client/app.js
@@ -18,6 +18,11 @@
 		controller: 'measurementsOverviewCtrl',
 		controllerAs: 'vm'
 	    })
+	    .when('/measurementOverview/:deviceId', {
+		templateUrl: 'measurements/measurementsOverview.view.html',
+		controller: 'measurementsOverviewCtrl',
+		controllerAs: 'vm'
+	    })
     	    .otherwise({redirectTo: '/'});
     }
 
