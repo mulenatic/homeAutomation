071a64db9fc662ec0082ba4153fc3933934e68e1
mulenatic <mulenatic@gmail.com>
2018-07-23 09:40:32 +0200

    Added service for measurements and tried to include in in app.

---
+++ app_client/services/measurementService.js
@@ -0,0 +1,20 @@
+(function() {
+
+    angular
+	.module('homeAutomationApp')
+	.service('measurementService', measurementService);
+
+    function measurementService($http) {
+
+	var getDeviceList = function () {
+
+	    return $http.get('/api/measurement/device');
+
+	}
+
+	return {
+	    getDeviceList: getDeviceList
+	}
+    }
+
+})();
